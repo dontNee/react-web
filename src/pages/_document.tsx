093033db777e785b4d8171f3c9979fd6babdef85
LiBeifeng <764020587@qq.com>
2023-06-01 11:28:46 +0800

    修改应用的标题

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>我的React网站</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
